da2fa1a2f69ff4ed9c59915f15dbfe1a5be57482
Alan Christian <alanchristiancreator@hotmail.com>
2021-09-13 00:08:54 -0300

    Adicionando model e controller de produtos

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 // const path = require('path')
 
-// const db = require('./database')
+const db = require('./database/db')
 const routes = require('./routes/routes')
 
 const app = express()
 
 // conexão com o banco de dados
-// db.connect()
+db.connect()
 
 // habilita server para receber dados via post
 app.use(express.urlencoded({ extended: true }))
@@ -16,5 +16,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api', routes)
 
 // executando o servidor
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+const PORT = process.env.PORT || 8080
+const HOSTNAME = 'localhost'
+
+app.listen(PORT, HOSTNAME, () =>
+  console.log(`Server started on http://${HOSTNAME}:${PORT}`)
+)
